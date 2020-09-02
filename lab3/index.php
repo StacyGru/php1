4f87593b67065f0fcb8911aa4136bd26b4c22663
StacyGru <stacy.gru@gmail.com>
2020-09-02 20:45:40 +0400

    Лаба 3 (статическая)

---
+++ lab3/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Грушевская Анастасия Сергеевна, 191-321. Лабораторная работа №А-3. Использование GET-параметров в ссылках. Виртуальная клавиатура.</title>
+        <meta charset="utf-8">
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700&display=swap" rel="stylesheet">
+        <style>
+            <?php
+            require "style.css";
+            ?>
+            </style>
+        </head>
+    <body>
+        <header>
+            <img src="img/logo.png" alt="logo">
+            <h1>Грушевская Анастасия Сергеевна, 191-321.<br>
+                <u>Лабораторная работа №А-3. Использование GET-параметров в ссылках. Виртуальная клавиатура.</u></h1>
+            </header>
+
+        <main>
+
+            <div class="container">
+
+                <div class="item window">
+                    </div>
+                <div class="item num">1</div>
+                <div class="item num">2</div>
+                <div class="item num">3</div>
+                <div class="item num">4</div>
+                <div class="item num">5</div>
+                <div class="item num">6</div>
+                <div class="item num">7</div>
+                <div class="item num">8</div>
+                <div class="item num">9</div>
+                <div class="item num">0</div>
+                <div class="item reset">СБРОС</div>
+                
+                </div>
+                
+            </main>
+
+        <footer>
+            Подвал
+            </footer>
+        </body>
+    </html>
