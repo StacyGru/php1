67b334907d06661418647d59df603982891f89be
StacyGru <stacy.gru@gmail.com>
2020-09-06 21:07:38 +0400

    Лаба 3 (готова)

--- lab3/index.php
+++ lab3/index.php
@@ -14,7 +14,7 @@
     <body>
         <header>
             <?php
-            if (!isset($_GET['key']))
+            if (!isset($_GET['clicks']))
                 echo '<img src="../lab3/img/logo.png" alt="logo">';
             else
                 echo '<img src="../img/logo.png" alt="logo">';
@@ -29,33 +29,42 @@
 
                 <div class="item window">
                 <?php                                                                                                                                                              
+                    if (!isset($_GET['clicks']))
+                        $_GET['clicks'] = 0;
+                    else
+                        $_GET['clicks']++;
+                   
                     if (!isset($_GET['store']))
-                        $_GET['store'] = '';        
-                                                                 
-                    if (isset($_GET['key']))                                                                                                                                               
+                        $_GET['store'] = '';   
+                        
+                    $clicks = 0;
+                
+                    if (isset($_GET['key']))                                                                                                                
                         $_GET['store'] .= $_GET['key'];      
                     
                     echo $_GET['store'];                                                                             
                     ?>                                                                                                                                    
                     </div>
-                <a class="item num" href="/lab3/index.php/?key=1&store=<?php echo $_GET['store']; ?>">1</a>
-                <a class="item num" href="/lab3/index.php/?key=2&store=<?php echo $_GET['store']; ?>">2</a>
-                <a class="item num" href="/lab3/index.php/?key=3&store=<?php echo $_GET['store']; ?>">3</a>
-                <a class="item num" href="/lab3/index.php/?key=4&store=<?php echo $_GET['store']; ?>">4</a>
-                <a class="item num" href="/lab3/index.php/?key=5&store=<?php echo $_GET['store']; ?>">5</a>
-                <a class="item num" href="/lab3/index.php/?key=6&store=<?php echo $_GET['store']; ?>">6</a>
-                <a class="item num" href="/lab3/index.php/?key=7&store=<?php echo $_GET['store']; ?>">7</a>
-                <a class="item num" href="/lab3/index.php/?key=8&store=<?php echo $_GET['store']; ?>">8</a>
-                <a class="item num" href="/lab3/index.php/?key=9&store=<?php echo $_GET['store']; ?>">9</a>
-                <a class="item num" href="/lab3/index.php/?key=0&store=<?php echo $_GET['store']; ?>">0</a>
-                <a class="item reset" href="/lab3/index.php">СБРОС</a>
+                <a class="item num" href="/lab3/index.php/?key=1&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">1</a>
+                <a class="item num" href="/lab3/index.php/?key=2&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">2</a>
+                <a class="item num" href="/lab3/index.php/?key=3&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">3</a>
+                <a class="item num" href="/lab3/index.php/?key=4&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">4</a>
+                <a class="item num" href="/lab3/index.php/?key=5&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">5</a>
+                <a class="item num" href="/lab3/index.php/?key=6&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">6</a>
+                <a class="item num" href="/lab3/index.php/?key=7&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">7</a>
+                <a class="item num" href="/lab3/index.php/?key=8&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">8</a>
+                <a class="item num" href="/lab3/index.php/?key=9&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">9</a>
+                <a class="item num" href="/lab3/index.php/?key=0&store=<?php echo $_GET['store']; ?>&clicks=<?php echo $_GET['clicks']; ?>">0</a>
+                <a class="item reset" href="/lab3/index.php/?clicks=<?php echo $_GET['clicks']; ?>">СБРОС</a>
                 
                 </div>
                 
             </main>
 
         <footer>
-            Подвал
+            <?php
+            echo 'Количество нажатий кнопок: '.$_GET['clicks'];
+            ?>
             </footer>
         </body>
     </html>
